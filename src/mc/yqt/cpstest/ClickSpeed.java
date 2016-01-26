975e0ce7c3d85abafa171502f3b9434782b32a6b
William L <William L>
2016-01-26 17:42:22 -0500

    Initial commit

---
+++ src/mc/yqt/cpstest/ClickSpeed.java
@@ -0,0 +1,144 @@
+package mc.yqt.cpstest;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers.EntityUseAction;
+
+public class ClickSpeed extends JavaPlugin implements Listener {
+
+	private static LinkedList<Test> tests = new LinkedList<Test>();
+	public static ProtocolManager PM;
+	
+	@Override
+	public void onEnable() {
+		PM = ProtocolLibrary.getProtocolManager();
+		
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		this.createPacketListener();
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		//clicks per second command
+		if(cmd.getName().equalsIgnoreCase("cps"))
+		{
+			
+			//command sender must be a player!
+			if(!(sender instanceof Player))
+			{
+				sender.sendMessage("You must be a player to do this!");
+				return true;
+			}
+			
+			Player p = (Player) sender;
+			
+			if(isInTest(p) != null)
+				//if player has a test currently active, don't start another
+				p.sendMessage("§cYou can't run multiple tests at once!");
+			else
+				//start a new test
+				tests.add(new Test(p));
+			
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		//on move, teleport the entity to new location
+		Test t;
+		if((t = isInTest(e.getPlayer())) != null)
+			t.sendZombieTPPacket();
+		
+	}
+	
+	@EventHandler
+	public void onTeleport(PlayerTeleportEvent e) {
+		//on teleport cancel the click speed test
+		Test t;
+		if((t = isInTest(e.getPlayer())) != null)
+		{
+			t.stop();
+			e.getPlayer().sendMessage("§cYou can't teleport during a click speed test! Test failed.");
+		}
+	}
+	
+	//method to create protocollib packet listener
+	public void createPacketListener() {
+		PM.addPacketListener(new PacketAdapter(this, PacketType.Play.Client.USE_ENTITY) 
+		{
+			@Override
+			public void onPacketReceiving(PacketEvent e) 
+			{
+				//handle incoming packet
+				if(e.getPacketType().equals(PacketType.Play.Client.USE_ENTITY) && e.getPacket().getEntityUseActions().read(0).equals(EntityUseAction.ATTACK))
+				{
+					//make sure it's valid for a click speed test
+					Test t;
+					
+					if((t = isInTest(e.getPlayer())) != null)
+					{
+						if(t.getClicks() == 0)
+							//if first click, start test
+							t.startTest();
+						else
+							//else increment num of clicks
+							t.incrementClicks();
+					}
+				}
+			}
+		});
+	}
+
+	/* STATIC METHODS */
+	//checks if a test for this user currently exists
+	public static Test isInTest(Player p) {
+		for(Test t : tests)
+			if(t.getPlayer().equals(p))
+				return t;
+			
+		return null;
+	}
+		
+	//removes a specified test from active tests list
+	public static void removeTest(Test t) {
+		Iterator<Test> it = tests.iterator();
+		
+		while(it.hasNext())
+			if(it.next().equals(t))
+			{
+				it.remove();
+				return;
+			}
+	}
+	
+	//gets location of zombie; right ahead of the player
+	public static Location getZombieLocation(Player p) {
+		return p.getLocation().getDirection().multiply(1).add(p.getLocation().toVector()).toLocation(p.getWorld());
+	}
+}
---
+++ src/mc/yqt/cpstest/Test.java
@@ -0,0 +1,234 @@
+package mc.yqt.cpstest;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers.TitleAction;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+
+public class Test {
+
+	/* test info */
+	private Player p; 
+	private int clicks = 0;
+	private int time = 0;
+	private long startms;
+	private BukkitTask test;
+	
+	/* entity info */
+	private int EID;
+	private Location loc;
+	
+	//construct the test
+	public Test(Player p) {
+		this.p = p;
+		
+		//send packet of an invisible giant zombie for the player to hit
+		this.sendZombieSpawnPacket();
+	}
+	
+	public Player getPlayer() {
+		return this.p;
+	}
+	
+	public int getClicks() {
+		return this.clicks;
+	}
+	
+	public int getEID() {
+		return this.EID;
+	}
+	
+	//start test loop, runs once every second for 10 seconds
+	public void startTest() {
+		this.startms = System.currentTimeMillis();
+		this.sendTitleCreatePackets();
+		this.incrementClicks();
+		
+		this.test = new BukkitRunnable() {
+			
+			@Override
+			public void run()
+			{
+				Test.sendTitleUpdatePacket(p, time);
+				
+				time++;
+
+				
+				if(time > 10)
+					stop();
+			}
+		}.runTaskTimer(ClickSpeed.getPlugin(ClickSpeed.class), 0L, 20L);
+	}
+	
+	//stops test loop
+	public void stopTest() {
+		this.test.cancel();
+	}
+	
+	//increment click counter
+	public void incrementClicks() {
+		this.clicks++;
+		this.sendSubtitleUpdatePacket();
+	}
+	
+	//handler to shut down tests
+	public void stop() {
+		this.stopTest();
+		this.sendZombieDestroyPacket();
+		ClickSpeed.removeTest(this);
+	}
+	
+	//sends the initial starting zombie creation packet
+	public void sendZombieSpawnPacket() {
+		
+		//set data watcher metadata
+		WrappedDataWatcher dw = new WrappedDataWatcher();
+		dw.setObject(0, (byte) 32); //sets metadata of zombie to invisibile
+		dw.setObject(4, (byte) 1); //sets zombie to silent
+		
+		
+		//set entity data
+		this.EID = new Random().nextInt();
+		this.loc = ClickSpeed.getZombieLocation(this.p);
+		
+		//create entity spawn packet
+		PacketContainer packet = ClickSpeed.PM.createPacket(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
+		
+		packet.getIntegers().write(0, this.EID); //in game entity ID
+		packet.getIntegers().write(1, 53); //giant zombie entity type ID
+		packet.getIntegers().write(2, (int) Math.floor(this.loc.getX() * 32.0D)); //entity X pos
+		packet.getIntegers().write(3, (int) Math.floor((this.loc.getY() + 1) * 32.0D)); //entity Y pos
+		packet.getIntegers().write(4, (int) Math.floor(this.loc.getZ() * 32.0D)); //entity Z pos
+		packet.getBytes().write(0, (byte) 0); //entity yaw (irrelevant)
+		packet.getBytes().write(1, (byte) 0); //entity head pitch (irrelevant)
+		packet.getBytes().write(2, (byte) 0); //entity head yaw (irrelevant)
+		packet.getIntegers().write(5, 0); //entity velocity X
+		packet.getIntegers().write(6, 0); //entity velocity Y
+		packet.getIntegers().write(7, 0); //entity velocity Z
+		packet.getDataWatcherModifier().write(0, dw); //entity metadata
+		
+		//try to send packet to client
+		try {
+			ClickSpeed.PM.sendServerPacket(this.p, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			
+			//if failed, cancel the test
+			this.p.sendMessage("§cFailed to start click speed test!");
+			ClickSpeed.removeTest(this);
+		}
+	}
+	
+	//sends a packet that tells the client to teleport the zombie
+	public void sendZombieTPPacket() {
+		//update zombie location
+		this.loc = ClickSpeed.getZombieLocation(this.p);
+		
+		PacketContainer packet = ClickSpeed.PM.createPacket(PacketType.Play.Server.ENTITY_TELEPORT);
+		
+		packet.getIntegers().write(0, this.EID); //in game entity ID
+		packet.getIntegers().write(1, (int) Math.floor(this.loc.getX() * 32.0D)); //entity X pos
+		packet.getIntegers().write(2, (int) Math.floor((this.loc.getY() + 1) * 32.0D)); //entity Y pos
+		packet.getIntegers().write(3, (int) Math.floor(this.loc.getZ() * 32.0D)); //entity Z pos
+		packet.getBytes().write(0, (byte) 0); //entity pitch (irrelevant)
+		packet.getBytes().write(1, (byte) 0); //entity yaw (irrelevant)
+		packet.getBooleans().write(0, false); //entity is not necessarily on ground
+		
+		try {
+			ClickSpeed.PM.sendServerPacket(this.p, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			
+			//if failed, cancel the test
+			this.p.sendMessage("§cProtocol code error, test failed!");
+			this.stop();
+		}
+		
+		
+	}
+	
+	//sends a packet that destroys the zombie
+	public void sendZombieDestroyPacket() {
+		PacketContainer packet = ClickSpeed.PM.createPacket(PacketType.Play.Server.ENTITY_DESTROY);
+		
+		packet.getIntegerArrays().write(0, new int[]{this.EID}); //integer array of entities to destroy, just one
+		
+		try {
+			ClickSpeed.PM.sendServerPacket(this.p, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//sends the title creation packets
+	public void sendTitleCreatePackets() {
+		//create blank title packet
+		PacketContainer p1 = ClickSpeed.PM.createPacket(PacketType.Play.Server.TITLE);
+		p1.getTitleActions().write(0, TitleAction.TITLE); //tells client we are setting the title
+		p1.getChatComponents().write(0, WrappedChatComponent.fromText("")); //write blank title
+		
+		//create subtitle packet
+		PacketContainer p2 = ClickSpeed.PM.createPacket(PacketType.Play.Server.TITLE);
+		p2.getTitleActions().write(0, TitleAction.SUBTITLE); //tells client we are setting subtitle
+		p2.getChatComponents().write(0, WrappedChatComponent.fromText("§e0 | 0.0 cps")); //write intro subtitle
+		
+		//create main subtitle packets, will run for the duration of the test
+		PacketContainer p3 = ClickSpeed.PM.createPacket(PacketType.Play.Server.TITLE);
+		p3.getTitleActions().write(0, TitleAction.TIMES); //tells client we are setting the times
+		p3.getIntegers().write(0, 0); //fade in for 0 ticks
+		p3.getIntegers().write(1, 40); //stay on screen for 12 seconds (every time title packet gets sent, time is reset)
+		p3.getIntegers().write(2, 10); //fade out for 10 ticks
+		
+		try {
+			ClickSpeed.PM.sendServerPacket(this.p, p1);
+			ClickSpeed.PM.sendServerPacket(this.p, p2);
+			ClickSpeed.PM.sendServerPacket(this.p, p3);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			
+			//if failed, cancel the test
+			this.p.sendMessage("§cProtocol code error, test failed!");
+			this.stop();
+		}
+	}
+	
+	//update title
+	public static void sendTitleUpdatePacket(Player p, int time) {
+		PacketContainer packet = ClickSpeed.PM.createPacket(PacketType.Play.Server.TITLE);
+		packet.getTitleActions().write(0, TitleAction.TITLE); //tells client we are setting title
+		packet.getChatComponents().write(0, WrappedChatComponent.fromText(((time < 7) ? "§e" : "§c") + (10 - time) + " seconds")); //write updated title
+		
+		try {
+			ClickSpeed.PM.sendServerPacket(p, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//update subtitle
+	public void sendSubtitleUpdatePacket() {
+		PacketContainer packet = ClickSpeed.PM.createPacket(PacketType.Play.Server.TITLE);
+		packet.getTitleActions().write(0, TitleAction.SUBTITLE); //tells client we are setting subtitle
+		packet.getChatComponents().write(0, WrappedChatComponent.fromText(((this.clicks < 75) ? "§e" : ((this.clicks < 100) ? "§d" : "§c")) + this.clicks + " | " 
+													+ Math.round((double) this.clicks / ((double) (System.currentTimeMillis() - this.startms) / 1000) * 10.0) / 10.0 + " cps")); //write updated subtitle
+		
+		try {
+			ClickSpeed.PM.sendServerPacket(this.p, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			
+			//if failed, cancel the test
+			this.p.sendMessage("§cProtocol code error, test failed!");
+			this.stop();
+		}
+	}
+}
